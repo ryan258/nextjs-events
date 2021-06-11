a08fb9797c329b407ff6f977759693b1f89e2376
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-11 15:18:38 -0500

    Create a Layout to wrap all site pages and give them a Header to navigate the site

---
+++ components/layout/MainHeader.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+
+import classes from './MainHeader.module.css'
+
+const MainHeader = (props) => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <Link href="/">NextEvents</Link>
+      </div>
+      <nav className={classes.navigation}>
+        <ul>
+          <Link href="/events">Browse All Events</Link>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default MainHeader
