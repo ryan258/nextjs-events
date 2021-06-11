5be233cd75e64b04fd82be10e259b21137cb1666
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-11 14:38:25 -0500

    Create components w/ styles to build out the home page w/ an EventList of featured events

---
+++ components/events/EventItem.js
@@ -0,0 +1,45 @@
+import Button from '../ui/Button'
+import DateIcon from '../icons/date-icon'
+import AddressIcon from '../icons/address-icon'
+import ArrowRightIcon from '../icons/arrow-right-icon'
+
+import classes from './EventItem.module.css'
+
+const EventItem = ({ title, image, date, location, id }) => {
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  })
+  const formattedAddress = location.replace(', ', '\n')
+  const exploreLink = `/events/${id}`
+
+  return (
+    <li className={classes.item}>
+      <img src={`/${image}`} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{formattedDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default EventItem
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import EventList from '../components/events/EventList'
+import { getFeaturedEvents } from '../dummy-data'
 
 const HomePage = () => {
+  const featuredEvents = getFeaturedEvents()
+
   return (
     <div>
-      <h1>beep</h1>
+      <EventList items={featuredEvents} />
     </div>
   )
 }
